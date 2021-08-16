1cb20d6ce35a043b8d42e49ad391e97147a02952
Ryan Brandt <rnb52@scarletmail.rutgers.edu>
2021-08-16 17:16:17 -0400

    kill dead interface

--- app/models/graphql/I_paginated_graphql_collection.py
+++
@@ -1,8 +0,0 @@
-from app.models.candidate import Candidate
-import graphene
-
-from .graphql_pagination import GraphQLPagination
-
-
-class IPaginatedGraphQLCollection(graphene.Interface):
-    pagination = graphene.Field(lambda: GraphQLPagination)
